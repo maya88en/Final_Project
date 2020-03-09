8ebe092bc9a30c21993063b5744b80e15d598960
maya88en <maya88en@gmail.com>
2020-03-09 19:54:20 +0200

    create microposts

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+end
